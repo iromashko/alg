2cc6f813146d660af8df4932a14fd2c426dff9c7
iromashko <iromashko@me.com>
2019-12-19 13:57:38 +0300

    basic refactor

--- sliding_window/00_basic.js
+++ sliding_window/00_basic.js
@@ -1,12 +1,16 @@
-function find_averages_of_subarrays(K, arr) {
+function find_averages_of_subarrays(K, array) {
   const result = [];
-  for (i = 0; i < arr.length - K + 1; i++) {
+  for (let i = 0; i < array.length - K + 1; i++) {
     sum = 0.0;
     for (j = i; j < i + K; j++) {
-      sum += arr[j];
+      sum += array[j];
     }
     result.push(sum / K);
   }
-
   return result;
 }
+
+// O(N * K)
+const result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
+console.log(`Averages of subarrays of size K: 2.2,2.8,2.4,3.6,2.8`);
+console.log(`Averages of subarrays of size K: ${result}`);
