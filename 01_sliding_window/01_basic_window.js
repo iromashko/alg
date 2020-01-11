79e67ec84879ba8c88acfd00db2fffd6e1a7b31f
iromashko <iromashko@me.com>
2020-01-11 23:41:09 +0300

    add all sliding windows

---
+++ 01_sliding_window/01_basic_window.js
@@ -0,0 +1,38 @@
+function findAveragesOfSubarrays(k, array) {
+  const result = [];
+  let windowSum = 0.0;
+  let windowStart = 0;
+  for (let windowEnd = 0; windowEnd < array.length; windowEnd++) {
+    windowSum += array[windowEnd];
+
+    if (windowEnd >= k - 1) {
+      result.push(windowSum / k);
+      windowSum -= array[windowStart];
+      windowStart++;
+    }
+  }
+  return result;
+}
+
+// function findAveragesOfSubarrays_(K, array) {
+//   const result = [];
+//   let windowSum = 0.0;
+//   let windowStart = 0;
+
+//   for (let windowEnd = 0; windowEnd < array.length; windowEnd++) {
+//     windowSum += array[windowEnd];
+
+//     if (windowEnd >= K - 1) {
+//       result.push(windowSum / K);
+//       windowSum -= array[windowStart];
+//       windowStart++;
+//     }
+//   }
+
+//   return result;
+// }
+
+// O(N)
+const result = findAveragesOfSubarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
+console.log(`Averages of subarrays of size K: 2.2,2.8,2.4,3.6,2.8`);
+console.log(`Averages of subarrays of size K: ${result}`);
