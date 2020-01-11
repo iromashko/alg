247f6dfa9f14af211568a4ece0a53a2f9367481a
iromashko <iromashko@me.com>
2020-01-12 01:25:12 +0300

    add arrays

--- sliding_window/01_basic_window.js
+++ sliding_window/01_basic_window.js
@@ -1,36 +1,20 @@
-function findAveragesOfSubarrays(k, array) {
+function findAveragesOfSubarrays_(K, array) {
   const result = [];
   let windowSum = 0.0;
   let windowStart = 0;
+
   for (let windowEnd = 0; windowEnd < array.length; windowEnd++) {
     windowSum += array[windowEnd];
 
-    if (windowEnd >= k - 1) {
-      result.push(windowSum / k);
+    if (windowEnd >= K - 1) {
+      result.push(windowSum / K);
       windowSum -= array[windowStart];
       windowStart++;
     }
   }
+
   return result;
 }
-
-// function findAveragesOfSubarrays_(K, array) {
-//   const result = [];
-//   let windowSum = 0.0;
-//   let windowStart = 0;
-
-//   for (let windowEnd = 0; windowEnd < array.length; windowEnd++) {
-//     windowSum += array[windowEnd];
-
-//     if (windowEnd >= K - 1) {
-//       result.push(windowSum / K);
-//       windowSum -= array[windowStart];
-//       windowStart++;
-//     }
-//   }
-
-//   return result;
-// }
 
 // O(N)
 const result = findAveragesOfSubarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]);
--- sliding_window/03_max_sub_array_of_size_k.js
+++ sliding_window/03_max_sub_array_of_size_k.js
@@ -1,31 +1,15 @@
-function maxSubArrayOfSizeK(k, array) {
+function max_sub_array_of_size_k(k, arr) {
   let maxSum = 0;
   let windowSum = 0;
-  for (let i = 0; i < array.length - k + 1; i++) {
+  for (i = 0; i < arr.length - k + 1; i++) {
     windowSum = 0;
-    for (let j = i; j < i + k; j++) {
-      windowSum += array[j];
+    for (j = i; j < i + k; j++) {
+      windowSum += arr[j];
     }
-
     maxSum = Math.max(maxSum, windowSum);
   }
-
   return maxSum;
 }
-
-// function max_sub_array_of_size_k(k, arr) {
-//   let maxSum = 0,
-//     windowSum = 0;
-
-//   for (i = 0; i < arr.length - k + 1; i++) {
-//     windowSum = 0;
-//     for (j = i; j < i + k; j++) {
-//       windowSum += arr[j];
-//     }
-//     maxSum = Math.max(maxSum, windowSum);
-//   }
-//   return maxSum;
-// }
 
 console.log(
   `Maximum sum of a subarray of size K: ${maxSubArrayOfSizeK(3, [
