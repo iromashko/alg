916cd9d442c17a3ba7328a335864234fa82466ce
iromashko <iromashko@me.com>
2020-03-08 00:15:30 +0300

    refac

--- leetcode/239. Sliding Window Maximum.js
+++ leetcode/239. Sliding Window Maximum.js
@@ -2,8 +2,7 @@ function maxSlidingWindow(array, k) {
   const result = [];
   const queue = [];
   for (let i = 0; i < array.length; i++) {
-    while (queue.length - 1 >= 0 && array[i] > queue[queue.length - 1])
-      queue.pop();
+    while (queue.length && array[i] > queue[queue.length - 1]) queue.pop();
     queue.push(array[i]);
     const j = i + 1 - k;
     if (j >= 0) {
