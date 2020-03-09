3fedc2b1ade69e5073af096c6d1c3c0e747703d1
iromashko <iromashko@me.com>
2020-03-09 08:37:37 +0300

    refac: four number sum

--- algoexpert/Four Number Sum.js
+++ algoexpert/Four Number Sum.js
@@ -1,7 +1,7 @@
 function fourNumberSum(array, targetSum) {
   const allPairSums = {};
   const quadruplets = [];
-  for (let i = 1; i < array.length - 1; i++) {
+  for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       const currentSum = array[i] + array[j];
       const difference = targetSum - currentSum;
