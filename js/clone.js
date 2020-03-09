a3cfdf61f9a977d72d24170989ffe457214e07b9
iromashko <iromashko@me.com>
2020-03-09 10:22:20 +0300

    add clone object

--- js/clone.js
+++ js/clone.js
@@ -1,6 +1,14 @@
 const clone = items =>
   items.map(item => (Array.isArray(item) ? clone(item) : item));
 
+const cloneObj = obj =>
+  Object.getPrototypeOf(obj) !== Object.prototype
+    ? obj
+    : Object.keys(obj).reduce(
+        (acc, key) => ((acc[key] = clone(obj[key])), acc),
+        {}
+      );
+
 var nestedArray = [1, [2], 3];
 var arrayCopy = clone(nestedArray);
 // Make some changes
