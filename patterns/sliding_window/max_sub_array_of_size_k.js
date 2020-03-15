d2a57a2fc3255de651d2b0454d75c3ab36c5dc85
iromashko <iromashko@me.com>
2020-03-15 09:47:45 +0300

    refactor window

--- patterns/sliding_window/max_sub_array_of_size_k.js
+++ patterns/sliding_window/max_sub_array_of_size_k.js
@@ -1,14 +1,35 @@
-function maxSubarrayOfSizeK(k, array) {
-  let maxSum = 0.0;
+function maxSubarrayOfSizeK(array, k) {
+  let maxSum = 0;
   let windowSum = 0;
-  for (let i = 0; i < array.length - k + 1; i++) {
-    windowSum = 0;
-    for (let j = i; j < i + k; j++) {
-      windowSum += array[j];
+  let windowStart = 0;
+
+  for (windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+    if (windowEnd >= k - 1) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= arr[windowStart];
+      windowStart += 1;
     }
-    maxSum = Math.max(maxSum, windowSum);
   }
   return maxSum;
 }
 
-module.exports = maxSubarrayOfSizeK;
+console.log(
+  `Maximum sum of a subarray of size K: ${maxSubarrayOfSizeK(3, [
+    2,
+    1,
+    5,
+    1,
+    3,
+    2
+  ])}`
+);
+console.log(
+  `Maximum sum of a subarray of size KL ${maxSubarrayOfSizeK(2, [
+    2,
+    3,
+    4,
+    1,
+    5
+  ])}`
+);
