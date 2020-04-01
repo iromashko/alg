2ff6c75b515f6ab8546bd73437a3ccb5bfb97280
iromashko <iromashko@me.com>
2020-04-01 06:55:45 +0300

    levelOrder refactor

--- leetcode/102. Binary Tree Level Order Traversal.js
+++ leetcode/102. Binary Tree Level Order Traversal.js
@@ -1,13 +1,12 @@
-var levelOrder = function(root) {
-  let res = [];
-  helper(root, 0, res);
-  return res;
-};
-
-var helper = function(node, level, res) {
+function levelOrder(root) {
+  let result = [];
+  levelOrderHelper(root, 0, result);
+  return result;
+}
+function levelOrderHelper(node, level, result) {
   if (!node) return;
-  if (!res[level]) res[level] = [];
-  res[level].push(node.val);
-  helper(node.left, level + 1, res);
-  helper(node.right, level + 1, res);
-};
+  if (!result[level]) result[level] = [];
+  result[level].push(node.val);
+  levelOrderHelper(node.left, level + 1, result);
+  levelOrderHelper(node.right, level + 1, result);
+}
