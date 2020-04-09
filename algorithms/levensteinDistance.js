72fc8606519433aabed5079b3d473b1a01da088d
iromashko <iromashko@me.com>
2020-04-09 13:42:35 +0300

    update

--- algorithms/levensteinDistance.js
+++ algorithms/levensteinDistance.js
@@ -2,14 +2,14 @@ function levenshteinDistance(a, b) {
   const distanceMatrix = Array(b.length + 1)
     .fill(null)
     .map(() => Array(a.length + 1).fill(null));
-  for (let i = 0; i <= a.length; i += 1) {
+  for (let i = 0; i <= a.length; i++) {
     distanceMatrix[0][i] = i;
   }
-  for (let j = 0; j <= b.length; j += 1) {
+  for (let j = 0; j <= b.length; j++) {
     distanceMatrix[j][0] = j;
   }
-  for (let j = 1; j <= b.length; j += 1) {
-    for (let i = 1; i <= a.length; i += 1) {
+  for (let j = 1; j <= b.length; j++) {
+    for (let i = 1; i <= a.length; i++) {
       const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
       distanceMatrix[j][i] = Math.min(
         distanceMatrix[j][i - 1] + 1,
