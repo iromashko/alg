010dbd497811c7c39d21bd708d11b6bc61b5b2d3
iromashko <iromashko@me.com>
2020-05-12 16:19:43 +0300

    fix nodeDepths

--- nodeDepths.js
+++ nodeDepths.js
@@ -1,7 +1,6 @@
-function nodeDepths(tree, count = 0) {
-    if (!tree) return count;
-    count++;
-    let left = nodeDepths(tree.left, count);
-    let right = nodeDepths(tree.right, count);
-    return left + right;
+function nodeDepths(tree, depth = 0) {
+    if (!tree) return 0;
+    return (
+        depth + nodeDepths(tree.left, depth + 1) + nodeDepths(tree.right, depth + 1)
+    );
 }
