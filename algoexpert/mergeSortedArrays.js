1deb9a4483bf0f876896a267ff66663453536b46
iromashko <iromashko@me.com>
2020-05-27 00:44:20 +0300

    mergeSortedArrays temp

---
+++ algoexpert/mergeSortedArrays.js
@@ -0,0 +1,86 @@
+function mergeSortedArrays(arrays) {
+  const sortedList = [];
+  const smallestItems = [];
+  for (let arrayIdx = 0; arrayIdx < array.length; arrayIdx++) {
+    smallestItems.push({
+      arrayIdx,
+      elementIdx: 0,
+      num: arrays[arrayIdx][0],
+    });
+  }
+  const minHeap = new minHeap(smallestItems);
+  while (!minHeap.isEmpty()) {
+    const smallestItem = minHeap.remove();
+    const { arrayIdx, elementIdx, num } = smallestItem;
+    sortedList.push(num);
+    if (elementIdx === arrays[arrayIdx].length - 1) continue;
+    minHeap.insert({
+      arrayIdx,
+      elementIdx: elementIdx + 1,
+      num: arrays[arrayIdx][elementIdx + 1],
+    });
+  }
+  return sortedList;
+}
+class MinHeap {
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+  buildHeap(array) {
+    const firstParentIdx = Math.floor((array.length - 2) / 2);
+    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, array.length - 1, array);
+    }
+    return array;
+  }
+  siftDown(currentIdx, endIdx, heap) {
+    let childOneIdx = currentIdx * 2 + 1;
+    while (childOneIdx <= endIdx) {
+      const childTwoIdx =
+        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+      let idxToSwap;
+      if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+        idxToSwap = childTwoIdx;
+      } else {
+        idxToSwap = childOneIdx;
+      }
+      if (heap[idxToSwap] < heap[currentIdx]) {
+        [heap[currentIdx], heap[idxToSwap]] = [
+          heap[idxToSwap],
+          heap[currentIdx],
+        ];
+        currentIdx = idxToSwap;
+        childOneIdx = currentIdx * 2 + 1;
+      } else {
+        return;
+      }
+    }
+  }
+  siftUp(currentIdx, heap) {
+    let parentIdx = Math.floor((currentIdx - 1) / 2);
+    while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+      [heap[currentIdx], heap[parentIdx]] = [heap[parentIdx], heap[currentIdx]];
+      currentIdx = parentIdx;
+      parentIdx = Math.floor((currentIdx - 1) / 2);
+    }
+  }
+  peek() {
+    return this.heap[0];
+  }
+  remove() {
+    [this.heap[0], this.heap[this.heap.length - 1]] = [
+      this.heap[this.heap.length - 1],
+      this.heap[0],
+    ];
+    const valueToRemove = this.heap.pop();
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return valueToRemove;
+  }
+  insert(value) {
+    this.heap.push(value);
+    this.siftUp(this.heap.length - 1, this.heap);
+  }
+}
