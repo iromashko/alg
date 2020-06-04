f8be1bea417aaf74c70f52d2b244b2ceaf8601de
iromashko <iromashko@me.com>
2020-06-04 22:04:25 +0300

    iterative inorder traversal

---
+++ algoexpert/iterativeInOrderTraversal.js
@@ -0,0 +1,22 @@
+function iterativeInOrderTraversal(tree, callback) {
+  let previousNode = null;
+  let currentNode = tree;
+  while (currentNode !== null) {
+    let nextNode;
+    if (!previousNode || previousNode === currentNode.parent) {
+      if (currentNode.left) {
+        nextNode = currentNode.left;
+      } else {
+        callback(currentNode);
+        nextNode = currentNode.right || currentNode.parent;
+      }
+    } else if (previousNode === currentNode.left) {
+      callback(currentNode);
+      nextNode = currentNode.right || currentNode.parent;
+    } else {
+      nextNode = currentNode.parent;
+    }
+    previousNode = currentNode;
+    currentNode = nextNode;
+  }
+}
