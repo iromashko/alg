5cd78abc7af624ceb9efd9b7f5a2278bc6dd1af6
iromashko <iromashko@me.com>
2020-06-15 15:31:57 +0300

    interweaving strings

--- algoexpert/interweavingStrings.ts
+++ algoexpert/interweavingStrings.ts
@@ -1,14 +1,21 @@
-function interweavingStrings(one, two, three) {
+export function interweavingStrings(one: string, two: string, three: string) {
   if (three.length !== one.length + two.length) return false;
   return areInterwoven(one, two, three, 0, 0);
 }
-function areInterwoven(one, two, three, i, j) {
+function areInterwoven(
+  one: string,
+  two: string,
+  three: string,
+  i: number,
+  j: number
+): boolean {
   const k = i + j;
   if (k === three.length) return true;
   if (i < one.length && one[i] === three[k]) {
     if (areInterwoven(one, two, three, i + 1, j)) return true;
   }
-  if (j < two.length && two[j] === three[k])
+  if (j < two.length && two[j] === three[k]) {
     return areInterwoven(one, two, three, i, j + 1);
+  }
   return false;
 }
