cbbc7e305e2ded462ef366336445b8aefe7295c3
iromashko <iromashko@me.com>
2020-06-15 15:19:52 +0300

    minrewards

--- algoexpert/minRewards.ts
+++ algoexpert/minRewards.ts
@@ -1,6 +1,6 @@
-function minRewards(scores) {
-  const rewards = scores.map((_) => 1);
-  for (let i = 1; i < scores.length; i++) {
+export function minRewards(scores: number[]) {
+  const rewards: number[] = scores.map((_) => 1);
+  for (let i = 1; i <= scores.length; i++) {
     if (scores[i] > scores[i - 1]) rewards[i] = rewards[i - 1] + 1;
   }
   for (let i = scores.length - 2; i >= 0; i--) {
