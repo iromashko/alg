02542ec8d6655e09e5e30bb76a4c4934862fc4e2
iromashko <iromashko@me.com>
2020-06-15 14:50:15 +0300

    knapsack

--- algoexpert/knapsackProblem.ts
+++ algoexpert/knapsackProblem.ts
@@ -1,13 +1,13 @@
-function knapsackProblem(items, capacity) {
-  const knapsackValues = [];
-  for (let i = 0; i < items.length + 1; i++) {
+export function knapsackProblem(items: number[][], capacity: number) {
+  const knapsackValues: number[][] = [];
+  for (let i = 0; i <= items.length; i++) {
     const row = new Array(capacity + 1).fill(0);
     knapsackValues.push(row);
   }
-  for (let i = 1; i < items.length + 1; i++) {
-    const currentWeight = items[i - 1][1];
-    const currentValue = items[i - 1][0];
-    for (let c = 0; c < capacity + 1; c++) {
+  for (let i = 1; i <= items.length; i++) {
+    const currentWeight: number = items[i - 1][1];
+    const currentValue: number = items[i - 1][0];
+    for (let c = 1; c <= capacity; c++) {
       if (currentWeight > c) {
         knapsackValues[i][c] = knapsackValues[i - 1][c];
       } else {
@@ -20,20 +20,20 @@ function knapsackProblem(items, capacity) {
   }
   return [
     knapsackValues[items.length][capacity],
-    getKnapsackItems(knapsackValues, items)
+    getKnapsackItems(knapsackValues, items),
   ];
 }
-function getKnapsackItems(knapsackValues, items) {
-  const sequence = [];
+function getKnapsackItems(knapsackValues: number[][], items: number[][]) {
+  let sequence: number[] = [];
   let i = knapsackValues.length - 1;
   let c = knapsackValues[0].length - 1;
   while (i > 0) {
     if (knapsackValues[i][c] === knapsackValues[i - 1][c]) {
-      i -= 1;
+      i--;
     } else {
       sequence.unshift(i - 1);
       c -= items[i - 1][1];
-      i -= 1;
+      i--;
     }
     if (c === 0) break;
   }
