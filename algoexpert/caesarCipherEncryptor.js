847b3b5aa2aad81ad21557b49d019fc83ffe429f
iromashko <iromashko@me.com>
2020-07-31 12:17:33 +0300

    caesar encryptor

--- algoexpert/caesarCipherEncryptor.js
+++ algoexpert/caesarCipherEncryptor.js
@@ -1,15 +1,13 @@
 function caesarCipherEncryptor(string, key) {
-    const newLetters = [];
-    const newKey = key % 26;
-    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-    for (const letter of string) {
-        newLetters.push(getNewLetters(letter, newKey, alphabet));
-    }
-    return newLetters.join("");
+  const newLetters = [];
+  const newKey = key % 26;
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  for (const letter of string) {
+    newLetters.push(getNewLetters(letter, newKey, alphabet));
+  }
+  return newLetters.join('');
 }
 function getNewLetters(letter, key, alphabet) {
-    const newLetterCode = alphabet.indexOf(letter) + key;
-    return newLetterCode <= 25
-        ? alphabet[newLetterCode]
-        : alphabet[-1 + (newLetterCode % 25)];
+  const newLetterCode = alphabet.indexOf(letter) + key;
+  return alphabet[newLetterCode % 26];
 }
